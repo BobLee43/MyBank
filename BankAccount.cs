cc35efdbecf60ae912a70fa83169194c1309ceee
BobLee43 <127638784+BobLee43@users.noreply.github.com>
2024-11-13 16:33:01 -0800

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+        public int Number {  get; set; }
+        public string Owner { get; set; }   
+        public decimal Balance { get; set; }
+
+        public void Credit(decimal amount, DateTime tranDate, string note)
+        {
+        
+        } //Credit()
+
+        public void Debit(decimal amount, DateTime tranDate, string note) { 
+        
+        } //Debit()
+
+        public BankAccount(String name, decimal openBalance) 
+        {
+            Owner = name;
+            Balance = openBalance;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MyBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBank
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            var newAccount = new BankAccount("Larry", 100);
+            Console.WriteLine($"New Account for {newAccount.Owner} with balance of {newAccount.Balance}");
+            Console.ReadLine();
+        }
+    }
+}
